f7c3e86941d3a56f9e91c0c2eb42595d0e672533
Matthew McLean <matthewmclean21@gmail.com>
2018-11-12 20:58:39 -0800

    Add test user model, route, and view
    The user model is the primary piece we might want to keep here, the rest was to review general javascript/express/handlebars knowledge

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 Example = require("../models/example");
+User = require("../models/user")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,6 +17,28 @@ router.post('/', function(req, res, next) {
       res.redirect('/');
     }
   })
+});
+
+router.post('/addUser', function(req, res, next) {
+  console.log(req.body.username);
+  User.create(
+    {
+      username: req.body.username, 
+      password: req.body.password, 
+      email: req.body.email, 
+      address: {
+        street: req.body.street,
+        city: req.body.city,
+        state: req.body.state,
+        zipcode: req.body.zipcode
+      } 
+    }, function(err, id) {
+      if(err) {
+        console.log(err);
+      } else {
+        res.redirect('/');
+      }
+  });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+Users = require("../models/user");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  Users.find({}, function(err, users) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.render('users', {users: users});
+    }
+  });
 });
 
 module.exports = router;
