8c4dd0fdbabb8da8f1c6913ff224ce20fba7f1d4
Kevin Ha <38023023+kha4@users.noreply.github.com>
2018-11-22 15:27:26 -0800

    Add files via upload

---
+++ routes/login.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+Example = require("../models/example");
+Donor = require("../models/donor");
+Address = require("../models/address");
+Charity = require("../models/charity");
+
+/* GET login page. */
+router.get('/', function(req, res, next) {
+  res.render('login');
+});
+
+router.post('/signin', function(req, res, next) {
+	console.log(req.body);
+	Charity.findOne({}, function(err, result){
+		if(err){
+			Donor.findOne({}, function(err, result){
+				if(err){
+					console.log(err);
+					res.render('/login', { message: 'Username not found' });
+				} else {
+					if(req.body.pWord == result.password) {
+						res.redirect('index');
+					} else {
+						res.render('/login', { message: 'Incorrect password' });
+					}
+				}
+			});
+		} else {
+			if(req.body.pWord == result.password){
+				res.redirect('index');
+			} else {
+				res.render('/login', { message: 'Incorrect password' });
+			}
+		  }
+	});	
+});
